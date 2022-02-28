116d83b5d1d84a6ad101f3fef5fa01ad6208bb74
Benson Ngoy <bngoy@redhat.com>
2022-02-28 16:11:42 +0100

    adding ssh test

--- pkg/cloudclient/aws/private.go
+++ pkg/cloudclient/aws/private.go
@@ -258,7 +258,7 @@ func generateUserData(variables map[string]string) (string, error) {
 func (c *Client) findUnreachableEndpoints(ctx context.Context, instanceID string) {
 	// Compile the regular expressions once
 	reVerify := regexp.MustCompile(userdataEndVerifier)
-	reUnreachableErrors := regexp.MustCompile(`Unable to reach (\S+)`)
+	reUnreachableErrors := regexp.MustCompile(`unable to reach (\S+)`)
 
 	latest := true
 	input := ec2.GetConsoleOutputInput{
